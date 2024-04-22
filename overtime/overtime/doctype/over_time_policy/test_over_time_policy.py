397e0b8555c6ede8568ef9ca6ff8706a99040ef4
Muhammad-Ibrahim-448 <ibrahimmujahid551@gmail.com>
2024-04-22 22:38:32 +0500

    Initial commit

---
+++ overtime/overtime/doctype/over_time_policy/test_over_time_policy.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, ibrahim and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestOverTimePolicy(FrappeTestCase):
+	pass
